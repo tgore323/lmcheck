0ad8300341449d3d874c8eb15a27118a628c5d4b
tgore323 <tgore323@gmail.com>
2022-04-29 12:23:32 -0700

    added variables

---
+++ lmcheck.py
@@ -0,0 +1,9 @@
+import time
+
+
+testing_url1 = https://mirror.crexio.com/linuxmint/isos/testing/
+testing_url2 = https://ftp.crifo.org/mint-cd/testing/
+
+stable_url1 = https://mirror.crexio.com/linuxmint/isos/stable/21/
+stable_url2 = https://ftp.crifo.org/mint-cd/stable/21/
+
