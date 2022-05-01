58e12cc4d213b946f8c45be79d119f6bf25c8dba
tgore323 <tgore323@gmail.com>
2022-05-01 13:31:35 -0700

    began work on saving hash for leter comparison. code's a mess rn

--- lmcheck.py
+++ lmcheck.py
@@ -3,11 +3,16 @@ import time
 import hashlib
 import os
 import requests
-#from urllib import response
-#from urllib.request import urlopen, Request
 
-# open file that contains URLs to check
+
+# open file that contains URLs to check and create list for hash values
 urls = open('urls.txt')
+'''
+or, you can just put the URLs in a list
+# urls = ['https://mirror.crexio.com/linuxmint/isos/testing/sha256sum.txt', 
+# 'https://ftp.crifo.org/mint-cd/testing/sha256sum.txt']
+'''
+hash_list = []
 
 # check URLs for response code, then check for contents
 lines = urls.readlines()
@@ -22,6 +27,8 @@ for line in lines:
         print(r.status_code)
         hash_object = hashlib.sha256(r.text.encode())
         print(hash_object.hexdigest())
+        store = hash_object.hexdigest()
+        hash_list.append(store)
     else:
         print("Fetching from " + line + " has FAILED!")
         # below commented lines are for testing purposes
@@ -30,4 +37,21 @@ for line in lines:
 
 # close file without changes
 urls.close()
+#print(store)
+#print(hash_list)
 
+# saves hash values to hidden tmp file for comparison
+temp = open('.tmp.txt', 'w+')
+for h in hash_list:
+    temp.write(h + '\n')
+
+# checks for tmp file. if it exists, compare contents
+if os.path.isfile('.tmp.txt') == True:
+    open('.tmp.txt', 'w+')
+    # create a list with each line in tmp file, then compare
+    before = []
+    after = []
+    if before == after:
+        print("No changes detected")
+    else:
+        print("Changes detected")
